2f90d2b8e3b4a0621af93c9aab6e439142534e15
Maksym Hniezdov <gnezdov_maksim@icloud.com>
2020-04-27 22:16:13 +0200

    title and color

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        primarySwatch: Colors.deepPurpleAccent,
+        primarySwatch: Colors.deepPurple,
       ),
       home: MyHomePage(title: 'LetsStudy'),
     );
