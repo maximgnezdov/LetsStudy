68b3aef50f9966e3d9d9d177cb56d4220fb47251
jackgkk <gukalo2001@gmail.com>
2020-04-27 22:51:35 +0300

    Changed the color

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
       home: MyHomePage(title: 'Flutter Application Home Page'),
     );
