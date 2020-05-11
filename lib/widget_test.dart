8e88854f430da9b709c4f8abe37688c44b72f7fb
RaffS <61879517+LiLiTS96@users.noreply.github.com>
2020-05-11 17:29:05 +0300

    hello world test

---
+++ lib/widget_test.dart
@@ -0,0 +1,5 @@
+testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+// Build our app and trigger a frame.
+await tester.pumpWidget(MyApp());
+expect(find.text('Hello World!'), findsOneWidget);
+});
