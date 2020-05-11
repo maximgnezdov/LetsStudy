27e73e37b552f6833ebc2f18ffce18e022257a99
RaffS <61879517+LiLiTS96@users.noreply.github.com>
2020-05-11 18:16:10 +0300

    added conversation pages and widgets

--- lib/widget_test.dart
+++
@@ -1,5 +0,0 @@
-testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
-// Build our app and trigger a frame.
-await tester.pumpWidget(MyApp());
-expect(find.text('Hello World!'), findsOneWidget);
-});
