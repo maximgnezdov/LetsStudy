e465bea7affd9d746fe447437e049730c279a0b6
RaffS <61879517+LiLiTS96@users.noreply.github.com>
2020-05-12 19:45:28 +0300

    Added Bottomsheet with ChatList

--- lib/Chat_menu/pages/ConversationPage.dart
+++ lib/Chat_menu/pages/ConversationPage.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/Chat_menu/widgets/ChatAppBar.dart';
 import 'package:flutterapp/Chat_menu/widgets/ChatListWidget.dart';
-import 'package:flutterapp/Chat_menu/widgets/InputWidget.dart';
 import 'package:flutterapp/Chat_menu/config/Palette.dart';
-import 'package:flutterapp/Chat_menu/pages/ConversationBottomSheet.dart';
 
 class ConversationPage extends StatefulWidget {
   @override
@@ -12,32 +10,17 @@ class ConversationPage extends StatefulWidget {
 }
 
 class _ConversationPageState extends State<ConversationPage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-            key: _scaffoldKey,
-            appBar: ChatAppBar(), // Custom app bar for chat screen
-            body: Container(
-              color: Palette.chatBackgroundColor,
-              child:Stack(children: <Widget>[
-              Column(
-                children: <Widget>[
-                  ChatListWidget(),
-                GestureDetector(
-                  child: InputWidget(),
-                  onPanUpdate: (details){
-                    if(details.delta.dy < 0){
-                      _scaffoldKey.currentState
-                          .showBottomSheet<Null>((BuildContext context) {
-                      return ConversationBottomSheet();
-                 });
-              }
-            })
-                ],
-              ),
-            ]))));
+    return Column(children: <Widget>[
+      Expanded(flex: 2, child: ChatAppBar()), // Custom app bar for chat screen
+      Expanded(
+          flex: 11,
+          child: Container(
+            color: Palette.chatBackgroundColor,
+            child: ChatListWidget(),
+          ))
+    ]);
   }
 }
