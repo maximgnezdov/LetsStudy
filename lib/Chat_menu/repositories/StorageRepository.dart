59f8f63aff26f0126ee72a81d828a7adee5b0d7a
RaffS <61879517+LiLiTS96@users.noreply.github.com>
2020-05-29 21:48:09 +0300

    Sing in page

--- lib/Chat_menu/repositories/StorageRepository.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:flutterapp/Chat_menu/providers/StorageProvider.dart';
-
-class StorageRepository{
-  StorageProvider storageProvider = StorageProvider();
-  Future<String> uploadImage(File file, String path) => storageProvider.uploadImage(file, path);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,24 @@
 import 'package:flutter/material.dart';
-import 'Chat_menu/config/Palette.dart';
-import 'Chat_menu/pages/ConversationPageSlide.dart';
-import 'Chat_menu/pages/RegisterPage.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutterapp/Chat_menu/pages/ConversationPageSlide.dart';
-import 'package:flutterapp/Chat_menu/repositories/AuthenticationRepository.dart';
-import 'package:flutterapp/Chat_menu/repositories/StorageRepository.dart';
-import 'package:flutterapp/Chat_menu/repositories/UserDataRepository.dart';
-import 'Chat_menu/blocs/authentication/Bloc.dart';
+import 'package:letsstudy/views/signin.dart';
 
 void main() {
-  //create instances of the repositories to supply them to the app
-  final AuthenticationRepository authRepository = AuthenticationRepository();
-  final UserDataRepository userDataRepository = UserDataRepository();
-  final StorageRepository storageRepository = StorageRepository();
-  runApp(
-    BlocProvider(
-      builder: (context) => AuthenticationBloc(
-          authenticationRepository: authRepository,
-          userDataRepository: userDataRepository,
-          storageRepository: storageRepository)
-        ..dispatch(AppLaunched()),
-      child: LetsStudy(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class LetsStudy extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
-        primaryColor: Palette.primaryColor,
+        primaryColor: Color(0xff145C9E),
+        scaffoldBackgroundColor: Color(0xff1F1F1F),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      //home: ConversationPageSlide(),
-      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-        builder: (context, state) {
-          if (state is UnAuthenticated) {
-            return RegisterPage();
-          } else if (state is ProfileUpdated) {
-            return ConversationPageSlide();
-          } else {
-            return RegisterPage();
-          }
-        },
-      ),
+      home: SingIn(),
     );
   }
 }
