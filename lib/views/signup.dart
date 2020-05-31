f25d29b642921d56a79e967352dabec66510295c
RaffS <61879517+LiLiTS96@users.noreply.github.com>
2020-05-31 23:04:24 +0300

    done

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,147 +1,172 @@
-import 'package:flutter/material.dart';
+import 'package:letsstudy/helper/helperfunctions.dart';
+import 'package:letsstudy/helper/theme.dart';
 import 'package:letsstudy/services/auth.dart';
-import 'package:letsstudy/widgets/widget.dart';
+import 'package:letsstudy/views/chatrooms.dart';
+import 'package:letsstudy/widget/widget.dart';
+import 'package:flutter/material.dart';
+import 'package:letsstudy/services/database.dart';
 
 class SignUp extends StatefulWidget {
+  final Function toggleView;
+  SignUp(this.toggleView);
+
   @override
   _SignUpState createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
+  TextEditingController emailEditingController = new TextEditingController();
+  TextEditingController passwordEditingController = new TextEditingController();
+  TextEditingController usernameEditingController =
+      new TextEditingController();
 
-  bool isLoading = false;
-
-  AuthMethods authMethods = new AuthMethods();
+  AuthService authService = new AuthService();
+  DatabaseMethods databaseMethods = new DatabaseMethods();
 
   final formKey = GlobalKey<FormState>();
-  TextEditingController userNameTextEditingController = new TextEditingController();
-  TextEditingController emailTextEditingController = new TextEditingController();
-  TextEditingController passwordTextEditingController = new TextEditingController();
+  bool isLoading = false;
 
-  signMeUp(){
+  singUp() async {
+
     if(formKey.currentState.validate()){
       setState(() {
+
         isLoading = true;
       });
 
-      authMethods.signUpWithEmailAndPassword(emailTextEditingController.text, 
-          passwordTextEditingController.text).then((val){
-            print("$val");
+      await authService.signUpWithEmailAndPassword(emailEditingController.text,
+          passwordEditingController.text).then((result){
+            if(result != null){
+
+              Map<String,String> userDataMap = {
+                "userName" : usernameEditingController.text,
+                "userEmail" : emailEditingController.text
+              };
+
+              databaseMethods.addUserInfo(userDataMap);
+
+              HelperFunctions.saveUserLoggedInSharedPreference(true);
+              HelperFunctions.saveUserNameSharedPreference(usernameEditingController.text);
+              HelperFunctions.saveUserEmailSharedPreference(emailEditingController.text);
+
+              Navigator.pushReplacement(context, MaterialPageRoute(
+                  builder: (context) => ChatRoom()
+              ));
+            }
       });
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
-      body: isLoading ? Container(
-        child: Center(child: CircularProgressIndicator()),
-      ) : SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height - 50,
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        validator: (val){
-                          return val.isEmpty || val.length < 2 ? "Please Provide UserName" : null;
-                        },
-                        controller: userNameTextEditingController,
-                        style: simpleTextStyle(),
-                        decoration: textFieldDecoration("username"),
-                      ),
-                      TextFormField(
-                        validator: (val){
-                          return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ?
+      body: isLoading ? Container(child: Center(child: CircularProgressIndicator(),),) :  Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            Spacer(),
+            Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    style: simpleTextStyle(),
+                    controller: usernameEditingController,
+                    validator: (val){
+                      return val.isEmpty || val.length < 3 ? "Enter Username 3+ characters" : null;
+                    },
+                    decoration: textFieldInputDecoration("username"),
+                  ),
+                  TextFormField(
+                    controller: emailEditingController,
+                    style: simpleTextStyle(),
+                    validator: (val){
+                      return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ?
                           null : "Enter correct email";
-                        },
-                        controller: emailTextEditingController,
-                        style: simpleTextStyle(),
-                        decoration: textFieldDecoration("email"),
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        validator: (val){
-                          return val.length > 6 ? null : "Enter Password 6+ characters";
-                        },
-                        controller: passwordTextEditingController,
-                        style: simpleTextStyle(),
-                        decoration: textFieldDecoration("password"),
-                      ),
-                    ],
+                    },
+                    decoration: textFieldInputDecoration("email"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    style: simpleTextStyle(),
+                    decoration: textFieldInputDecoration("password"),
+                    controller: passwordEditingController,
+                    validator:  (val){
+                      return val.length < 6 ? "Enter Password 6+ characters" : null;
+                    },
 
                   ),
-                ),
-                SizedBox(height: 8,),
-                Container(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                      child: Text("Forgot Password?", style: simpleTextStyle(),),
-                    ),
-                  ),
-
-                SizedBox(height: 8,),
-                GestureDetector(
-                    onTap: (){
-                     signMeUp();
-                   },
-                 child: Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [
-                            const Color(0xff007EF4),
-                            const Color(0xff2A75BC)
-                          ]
-                      ),
-                      borderRadius: BorderRadius.circular(30)
-                  ),
-                  child: Text("Sign Up", style: mediumTextStyle(),),
-                ),
+                ],
+              ),
             ),
-                SizedBox(height: 16,),
-                Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30)
-                  ),
-                  child: Text("Sign Up with Google", style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 17
-                  ),),
+            SizedBox(
+              height: 16,
+            ),
+            GestureDetector(
+              onTap: (){
+                singUp();
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(
+                      colors: [const Color(0xff007EF4), const Color(0xff2A75BC)],
+                    )),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "Sign Up",
+                  style: biggerTextStyle(),
+                  textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 16,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Already have account? ", style: mediumTextStyle(),),
-                    Text("SignIn now", style: TextStyle(
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                "Sign Up with Google",
+                style: TextStyle(fontSize: 17, color: CustomTheme.textColor),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already have an account? ",
+                  style: simpleTextStyle(),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    widget.toggleView();
+                  },
+                  child: Text(
+                    "SignIn now",
+                    style: TextStyle(
                         color: Colors.white,
-                        fontSize: 17,
-                        decoration: TextDecoration.underline
-                    ),)
-                  ],
+                        fontSize: 16,
+                        decoration: TextDecoration.underline),
+                  ),
                 ),
-                SizedBox(height: 50,),
               ],
             ),
-          ),
+            SizedBox(
+              height: 50,
+            )
+          ],
         ),
       ),
     );
+    ;
   }
 }
